0cc180c55ab39e3b4f4e2fcc4829031aa16b4f3c
Alif1507 <m.alifwahyudi2007@gmail.com>
2025-03-03 14:05:18 +0700

    added informasi

--- app.js
+++ app.js
@@ -7,4 +7,33 @@ window.addEventListener("scroll", () =>{
   ats.style.top = value * -0.9 + "px"
   bwa.style.top = value * -0.9 + "px"
   hero.style.marginTop = value * -0.9 + "px"
+  
+});
+
+let panahKanan = document.querySelector(".panah-kanan")
+let panahkiri = document.querySelector(".panah-kiri")
+
+panahKanan.addEventListener("click", () => {
+  document.querySelectorAll(".kabur").forEach((el) => {
+    el.style.transform = "translateX(-588px)";
+  });
+
+  panahKanan.style.opacity = "0"
+  panahKanan.style.pointerEvents = "none"
+  panahkiri.style.opacity = "1"
+  panahkiri.style.pointerEvents = "all"
+
+  
+});
+
+panahkiri.addEventListener("click", () => {
+  document.querySelectorAll(".kabur").forEach((el) => {
+    el.style.transform = "translateX(0px)";
+  });
+
+  panahKanan.style.opacity = "1"
+  panahKanan.style.pointerEvents = "all"
+  panahkiri.style.opacity = "0"
+  panahkiri.style.pointerEvents = "none"
+  
 });
